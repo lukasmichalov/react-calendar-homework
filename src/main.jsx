ca4bdcea19729fa943478fabfd7c17b48dc2f920
lukasmichalov <l.michalov.st@spseiostrava.cz>
2025-01-12 20:21:41 +0100

    Import Bootstrap CSS in main.jsx for styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Calendar from './pages/Calendar/Calendar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
